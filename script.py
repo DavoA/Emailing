118a9cd507e98bf7daf29fa66ffaa0a06060e865
student <student@node8.cluster.local>
2023-10-16 12:56:41 +0400

    Added finished project

--- script.py
+++ script.py
@@ -1,49 +1,81 @@
 #!/usr/bin/env python3
+"""
+    These are imported modules.
+"""
 import argparse
 import re
+import sys
+import os
 import smtplib
 from email.mime.text import MIMEText
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-e","--email", help="email")
-parser.add_argument("-t","--title", nargs='*', help="the title")
-parser.add_argument("-m","--message", nargs='*', help="the message")
-args = parser.parse_args()
-
- 
-
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
- 
-def check(email):
+def check_file_existence(fname):
+    """
+        This is created to check file exestence.
+    """
     answer = True
-    if(re.fullmatch(regex, email)):
-        print("Valid Email")
- 
-    else:
+    if not os.path.isfile(fname):
+        print(f"Input file does not exists. Please check {fname}")
+        answer = False
+    return answer
+
+def get_code(fname):
+    """
+        This is created to get code from txt file.
+    """
+    with open(fname,"r", encoding="utf-8") as mfile:
+        return mfile.readline().strip()
+
+def check(email):
+    """
+        This is created to check email validation.
+    """
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    answer = True
+    if not re.fullmatch(regex, email):
         print("Invalid Email")
         answer = False
     return answer
-checking = check(args.email)
-if not checking:
-    exit()
-
-
-subject = args.title
-body = args.message
-sender = "dvo2014dd@gmail.com"
-with open("app.txt", "r") as f:
-    password = f.readline().strip()
-recipient = args.email
 
 def send_email(subject, body, sender, recipient, password):
+    """
+        This is created to send the message.
+    """
     msg = MIMEText(' '.join(body))
     msg['Subject'] = ' '.join(subject)
     msg['From'] = sender
     msg['To'] = recipient
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login(sender, password)
-       smtp_server.sendmail(sender, recipient, msg.as_string())
+        try:
+            smtp_server.login(sender, password)
+            smtp_server.sendmail(sender, recipient, msg.as_string())
+        except smtplib.SMTPAuthenticationError:
+            print("Something wrong with the email and password. Please check! ")
+            sys.exit()
     print("Message sent!")
 
+def main():
+    """
+        This is the main.
+    """
+    fname = "app.txt"
+    checking_file = check_file_existence(fname)
+    if not checking_file:
+        print("Check your file existence!")
+        sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e","--email", help="email")
+    parser.add_argument("-t","--title", nargs='*', help="the title")
+    parser.add_argument("-m","--message", nargs='*', help="the message")
+    args = parser.parse_args()
+    checking_email = check(args.email)
+    if not checking_email:
+        sys.exit()
+    subject = args.title
+    body = args.message
+    sender = "dvo2014dd@gmail.com"
+    password = get_code(fname)
+    recipient = args.email
+    send_email(subject, body, sender, recipient, password)
 
-send_email(subject, body, sender, recipient, password)
+main()
